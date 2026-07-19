d50dc82183730be8d667e4f6564d94599c58c3da
Noceo200 <102965900+Noceo200@users.noreply.github.com>
2026-07-19 10:00:04 +0200

    Fix GIT_TAG variable substitution in Honda.cmake

--- robots/Honda.cmake
+++ robots/Honda.cmake
@@ -8,7 +8,7 @@ if(ROS_IS_ROS2)
     AddCatkinProject(
       ur_description
       GITHUB UniversalRobots/Universal_Robots_ROS2_Description
-      GIT_TAG origin/$ENV{ROS_DISTRO}
+      GIT_TAG origin/${ROS_DISTRO}
       WORKSPACE data_ws
     )
   else() # ROS1
